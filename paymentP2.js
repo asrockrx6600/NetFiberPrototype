7f1da166b1b98dbd90499a08e7b96239f62ee3e8
Davi <asrockrx6600@gmail.com>
2025-12-09 19:00:19 -0300

    nine one

--- paymentP2.js
+++ paymentP2.js
@@ -1,16 +1,36 @@
-const inputPlan = document.querySelectorAll('planoEscolhido')
-const buttons = document.querySelectorAll('.plan-one-button')
+const inputPlan = document.getElementById('#planoEscolhido')
+const buttons = document.querySelectorAll('.buttonP')
+
 
 buttons.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        const card = button.closest('.plan-one')
+
+        const speed = card.querySelector('.plan-value').innerText.trim()
+        const price = card.querySelector('.plan-price').innerText.trim()
+
+        const planoFinal = `${speed} - ${price}`
+
+        const planoUrl = encodeURIComponent(planoFinal)
+
+        window.location.href = `payment.html?plano=${planoUrl}`
+    })
+})
+
+
+
+/* button.addEventListener('click', () => {
         
-        // pega o card onde o botão foi clicado
+})*/
+
+
+/* // pega o card onde o botão foi clicado
         const cardPlan = button.closest('.plan-one')
 
         // pega o texto do preço dentro do card
         const price = cardPlan.querySelector('.plan-price').innerHTML
 
         // envia para o input
-        inputPlan.value = price
-    })
-})
+        inputPlan.value = price */
